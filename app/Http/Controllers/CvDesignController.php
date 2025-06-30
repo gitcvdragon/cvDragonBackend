da5e0e603b1da1cf9878da095b31fc115b1f5f59
simran <simrankumaridga@gmail.com>
2025-06-30 11:17:43 +0530

    fix: refactor allCvDesgin method to improve sectionOrder handling

--- app/Http/Controllers/CvDesignController.php
+++ app/Http/Controllers/CvDesignController.php
@@ -13,9 +13,22 @@ class CvDesignController extends Controller
     //Fetch Active Designs, Fonts, and Colors
     public function allCvDesgin()
     {
+        // $designs = ResourceProfileDesignCategory::with(['resourceProfileDesigns' => function ($query) {
+        //     $query->get()->each(function ($design) {
+        //         $design->sectionOrder = json_decode($design->sectionOrder, true);
+        //     });
+        // }])->where('status', 1)->get();
+
         $designs = ResourceProfileDesignCategory::with(['resourceProfileDesigns' => function ($query) {
             $query->get()->each(function ($design) {
-                $design->sectionOrder = json_decode($design->sectionOrder, true);
+                $decoded = json_decode($design->sectionOrder, true);
+
+                if (is_array($decoded) && isset($decoded[0]) && is_array($decoded[0])) {
+                    $flattened            = collect($decoded)->flatten()->values();
+                    $design->sectionOrder = $flattened;
+                } else {
+                    $design->sectionOrder = $decoded;
+                }
             });
         }])->where('status', 1)->get();
 
