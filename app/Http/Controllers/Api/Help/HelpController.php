3b6739e54227cca63ecdcd66bce5c7df75b3227b
simran <simrankumaridga@gmail.com>
2025-08-05 14:08:07 +0530

    Implement allHelp method in HelpController to return help resources

--- app/Http/Controllers/Api/Help/HelpController.php
+++ app/Http/Controllers/Api/Help/HelpController.php
@@ -4,8 +4,24 @@ namespace App\Http\Controllers\Api\Help;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HelpController extends Controller
 {
-    //
+    public function allHelp(Request $request)
+    {
+        // $id = $request->input('id');
+        // $authkey = $request->input('authkey');
+
+        // if (!$this->isValidUser($id, $authkey)) {
+        //     return response()->json(['status' => 'error', 'message' => 'Unauthorized'], 401);
+        // }
+
+        return response()->json([
+            'status' => 'success',
+            'help-videos' => DB::table('help-videos')->where('status', 1)->get(),
+            'help-faq' => DB::table('help-faq')->where('status', 1)->get(),
+            'notifications' => DB::table('notifications')->select('notificationID', 'displayImage')->where('status', 1)->get()
+        ]);
+    }
 }
--- routes/website.php
+++ routes/website.php
@@ -1,13 +1,8 @@
 <?php
 
-use App\Http\Controllers\AppConfigController;
-use App\Http\Controllers\CvCommonController;
-use App\Http\Controllers\CvDesignController;
-use App\Http\Controllers\CvProfileController;
-use App\Http\Controllers\CvSectionController;
-use App\Http\Controllers\OTPAuthController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Api\Help\HelpController;
 use App\Http\Controllers\Api\Menu\MenuController;
 
 use Illuminate\Support\Facades\Route;
 Route::get('/menu', [MenuController::class, 'groupByIndex']);
+Route::get('/help', [HelpController::class, 'allHelp']);
