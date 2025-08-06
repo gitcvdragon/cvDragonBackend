ad486f1ec63c94dd2b57250359816aab0c4ebbdd
simran <simrankumaridga@gmail.com>
2025-08-06 16:47:45 +0530

    response trait added in header

--- app/Http/Controllers/Api/website/HomeController.php
+++ app/Http/Controllers/Api/website/HomeController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
     use Illuminate\Support\Facades\DB;
     use Illuminate\Support\Facades\Validator;
+use App\Traits\ApiResponseTrait;
+
 class HomeController extends Controller
 {
 
