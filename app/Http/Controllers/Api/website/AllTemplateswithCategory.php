621f583bff1014768ed3d12e08653555f7e38fde
simran <simrankumaridga@gmail.com>
2025-08-19 18:16:41 +0530

    updated

--- app/Http/Controllers/Api/website/AllTemplateswithCategory.php
+++ app/Http/Controllers/Api/website/AllTemplateswithCategory.php
@@ -1,12 +1,16 @@
 <?php
 
 namespace App\Http\Controllers\Api\website;
-
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\DB;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Traits\ApiResponseTrait;
+use Illuminate\Support\Facades\Validator;
 class AllTemplateswithCategory extends Controller
 {
+    use ApiResponseTrait;
+
     public function getAllTemplatesWithCategory()
     {
         $categories = DB::table('resource_profile_design_categories')
