37e0494bc57ee00e21d8b82b363c3b74390538e7
simran <simrankumaridga@gmail.com>
2025-09-08 16:06:36 +0530

    imported the validator in headier

--- app/Http/Controllers/CvSectionController.php
+++ app/Http/Controllers/CvSectionController.php
@@ -6,6 +6,7 @@ use App\Models\ResourceSection;
 use App\Traits\ApiResponseTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class CvSectionController extends Controller
 {
