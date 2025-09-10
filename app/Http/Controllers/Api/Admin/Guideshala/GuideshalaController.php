ae2d106340db53f120abd4a0856baa56d1620c32
simran <simrankumaridga@gmail.com>
2025-09-10 12:15:28 +0530

    updated

--- app/Http/Controllers/Api/Admin/Guideshala/GuideshalaController.php
+++ app/Http/Controllers/Api/Admin/Guideshala/GuideshalaController.php
@@ -143,7 +143,7 @@ class GuideshalaController extends Controller
             $categories = DB::table('kc-main')
                 ->select('kcid as id', 'kcName as name')
                 ->where('status', 1)
-                ->where('sn','!=' ,0)
+                ->where('kcid','!=' ,0)
                 ->orderBy('kcName', 'asc')
                 ->get();
 
